24ba893b74ee2894875708f4d53f7223f273c5c0
Kasper Marstal <kaspermarstal@gmail.com>
2026-09-10 23:39:54 +0200

    refactor: any_datum coercion code style

--- plprql/src/spi.rs
+++ plprql/src/spi.rs
@@ -3,7 +3,7 @@ use crate::fun::Function;
 use crate::plprql::prql_to_sql;
 use pgrx::datum::numeric_support::error::Error as NumericError;
 use pgrx::pg_return_null;
-use pgrx::pg_sys::panic::ErrorReportable;
+use pgrx::pg_sys::panic::{ErrorReport, ErrorReportable};
 use pgrx::prelude::*;
 use pgrx::{IntoDatum, IntoHeapTuple, PgSqlErrorCode, PgTupleDesc, pg_sys};
 
@@ -17,9 +17,12 @@ fn report_numeric_coercion_error(error: NumericError, column: Option<usize>, tar
         NumericError::ConversionNotSupported(_) => PgSqlErrorCode::ERRCODE_FEATURE_NOT_SUPPORTED,
         _ => PgSqlErrorCode::ERRCODE_DATA_EXCEPTION,
     };
-    let context = column.map_or_else(|| "return value".to_string(), |index| format!("return column {index}"));
+    let context = match column {
+        Some(index) => format!("return column {index}"),
+        None => "return value".to_string(),
+    };
 
-    pgrx::pg_sys::panic::ErrorReport::new(
+    ErrorReport::new(
         sqlstate,
         format!("could not coerce numeric {context} to {target_type}: {error}"),
         pgrx::function_name!(),
@@ -28,21 +31,17 @@ fn report_numeric_coercion_error(error: NumericError, column: Option<usize>, tar
     unreachable!()
 }
 
-fn coerce_return_datum(
-    any_datum: Option<AnyDatum>,
-    target_oid: pg_sys::Oid,
-    column: Option<usize>,
-) -> Option<AnyDatum> {
+fn coerce_any_datum(any_datum: Option<AnyDatum>, target_oid: pg_sys::Oid, column: Option<usize>) -> Option<AnyDatum> {
     match (any_datum, target_oid) {
         (Some(AnyDatum::Numeric(value)), pg_sys::FLOAT4OID) => {
-            Some(AnyDatum::F32(f32::try_from(value).unwrap_or_else(|error| {
-                report_numeric_coercion_error(error, column, "float4")
-            })))
+            let converted =
+                f32::try_from(value).unwrap_or_else(|error| report_numeric_coercion_error(error, column, "float4"));
+            Some(AnyDatum::F32(converted))
         }
         (Some(AnyDatum::Numeric(value)), pg_sys::FLOAT8OID) => {
-            Some(AnyDatum::F64(f64::try_from(value).unwrap_or_else(|error| {
-                report_numeric_coercion_error(error, column, "float8")
-            })))
+            let converted =
+                f64::try_from(value).unwrap_or_else(|error| report_numeric_coercion_error(error, column, "float8"));
+            Some(AnyDatum::F64(converted))
         }
         (value, _) => value,
     }
@@ -67,8 +66,8 @@ impl IntoHeapTuple for Row {
                 .get(index)
                 .unwrap_or_else(|| pgrx::error!("missing return column {} in tuple descriptor", index + 1))
                 .atttypid;
-            let any_datum = coerce_return_datum(any_datum, target_oid, Some(index + 1));
-            match any_datum.into_datum() {
+            let coerced_any_datum = coerce_any_datum(any_datum, target_oid, Some(index + 1));
+            match coerced_any_datum.into_datum() {
                 Some(datum) => {
                     datums.push(datum);
                     is_nulls.push(false);
@@ -133,7 +132,7 @@ pub(crate) fn fetch_setof(function: &Function) -> impl FnOnce() -> Option<Vec<Op
                         .unwrap_or_report()
                         .value::<AnyDatum>()
                         .unwrap_or_report();
-                    coerce_return_datum(any_datum, target_oid, None)
+                    coerce_any_datum(any_datum, target_oid, None)
                 })
                 .collect::<Vec<Option<AnyDatum>>>();
 
@@ -158,7 +157,7 @@ pub(crate) fn fetch_row(function: &Function) -> pg_sys::Datum {
             .first()
             .get_one::<AnyDatum>()
             .unwrap_or_report();
-        coerce_return_datum(any_datum, target_oid, None).into_datum()
+        coerce_any_datum(any_datum, target_oid, None).into_datum()
     })
     .unwrap_or_else(|| unsafe { pg_return_null(function.call_info) })
 }
